b9f3a5abdd52925c955fcef6e32900c76ef64f55
Rodrigo Polo - W8.1 <Rodrigo Polo>
2014-02-06 03:13:08 -0600

    test 1

--- main.js
+++ main.js
@@ -9,28 +9,27 @@ var movie_scan = require('./lib/movie_scan');
 var mindbl = require('./lib/movie_info_db');
 
 
-// Mongoose - make connection
-mongoose.connect('mongodb://'+config.mongodb.host+':'+config.mongodb.port+'/'+config.mongodb.db,function(err) {
-		if(err){
-			process.stdout.write('Cannot connect to the DB.');
-			process.exit(1);
-		}
-});
-
-// mongoose models
-var models = {};
-
-// User model
-models.movies = require('./models/movies')(mongoose);
-
-
-
-
 mongod.check(function(err, installed){
 	if(installed){
-		oscom.print('MongoDB installed, starting on port:'+config.mongodb.port,true);
+		oscom.print('MongoDB installed, starting on port:'+config.mongodb.port);
 		mongod.runInBackground(config.mongodb.port,function(err, running){
 			oscom.print('MongoDB running on port: '+config.mongodb.port);
+
+
+			// Mongoose - make connection
+			mongoose.connect('mongodb://'+config.mongodb.host+':'+config.mongodb.port+'/'+config.mongodb.db,function(err) {
+					if(err){
+						process.stdout.write('Cannot connect to the DB.');
+						process.exit(1);
+					}
+			});
+
+			// mongoose models
+			var models = {};
+
+			// User model
+			models.movies = require('./models/movies')(mongoose);
+			
 
 			oscom.print('Scanning directory.');
 			movie_scan.scanAndInsert(config.path,models,function(err,ret){
