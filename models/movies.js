c466b87b1202c0a394d23ab122c14753358fefe2
Rodrigo Polo - W8.1 <Rodrigo Polo>
2014-02-10 05:25:57 -0600

    more metadata

--- models/movies.js
+++ models/movies.js
@@ -5,42 +5,55 @@ module.exports = function(mongoose) {
 
 	var schema = new Schema({
 		path: { 
-			type: String, 
-			unique: true,
-			index: true
+			type:		String, 
+			unique:		true,
+			index:		true
 		},
 		name: { 
-			type: String, 
-			index: true
+			type:		String, 
+			index:		true
 		},
-		ext: String,
+		ext:				String,
 
-		video_tracks: [],
-		audio_tracks: [],
-		subtitles: [],
-		bitrate: Number,
-		size: Number,
-		duration: Number,
+		video_tracks:		[],
+		audio_tracks:		[],
+		subtitles:			[],
+		bitrate:			Number,
+		size:				Number,
+		duration:			Number,
 
+		id: { 
+			type:		Number, 
+			index:		true
+		},
+		genres:				[],
 		imdb_id: { 
-			type: String,
-			index: true
+			type:		String,
+			index: 		true
 		},
-		original_title: String,
-		tagline: String,
-		overview: String,
-		release_date: Date,
-		backdrop_path: String,
-		poster_path: String,
-		vote_average: Number,
+		original_title:		String,
+		tagline:			String,
+		overview:			String,
+		release_date:		Date,
+		backdrop_path:		String,
+		poster_path:		String,
+		vote_average:		Number,
+
+		cast:				[],
+		crew:				[],
+
+		trailers: {
+			quicktime:		[],
+			youtube:		[]
+		},
 
 		mi_status: {
-			type: Boolean,
-			default: false,
+			type: 		Boolean,
+			default: 	false,
 		},
 		md_status: {
-			type: Boolean,
-			default: false,
+			type:		Boolean,
+			default:	false,
 		}
 	}, { 
 		collection: collection,
